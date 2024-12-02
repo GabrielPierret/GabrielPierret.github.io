026851f669b43e1219b50abe5872b351b2a19569
Gabriel <gpierret@et.esiea.fr>
2024-12-02 23:39:27 +0100

    Responsive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-
 function openModal(id) {
-    document.getElementById(id).style.display = 'block';
+  document.getElementById(id).style.display = "block";
 }
 
 function closeModal(id) {
-    document.getElementById(id).style.display = 'none';
+  document.getElementById(id).style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamburger = document.querySelector(".menu-hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuHamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("active"); // Affiche/cache la nav
+  });
+});
